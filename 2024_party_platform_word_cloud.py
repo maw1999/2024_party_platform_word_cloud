fa166d0588a1e61556cfae02b2f080522fe40444
maw1999 <30275207+maw1999@users.noreply.github.com>
2024-10-07 11:30:56 -0700

    Generate more concise word clouds
    Also, add support for variable word cloud sizes

--- 2024_party_platform_word_cloud.py
+++ 2024_party_platform_word_cloud.py
@@ -16,7 +16,7 @@ def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
         cmap(np.linspace(minval, maxval, n)))
     return new_cmap
 
-def generate_word_cloud(filename, color_map):
+def generate_word_cloud(filename, color_map, max_words):
 	all_content = ""
 	with pdfplumber.open(filename) as pdf:
 		for page in pdf.pages:
@@ -25,7 +25,7 @@ def generate_word_cloud(filename, color_map):
 			
 	# Filter out words not directly related to policy, common terms, and words that add noise to word maps.
 	single_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "S"]
-	odd_fragments_and_common_terms = ["RE", "ll", "NN", "AANNDD", "TTHHEE", "VE", "CHAPTER", "YEAR", "YEARS", "PLATFORM", "POLICY", "POLICIES", "PEOPLE", "COUNTRY", "NATION", "NATIONAL", "WORLD", "TERM", "WASHINGTON", "FEDERAL", "GOVERNMENT", "INCLUDING", "PARTY", "PRESIDENT", "CONGRESS", "ADMINISTRATION", "WILL", "STATE", "UNITED", "STATES", "AMERICA", "AMERICAN", "AMERICANS"]
+	odd_fragments_and_common_terms = ["RE", "ll", "NN", "AANNDD", "TTHHEE", "VE", "CHAPTER", "YEAR", "YEARS", "FIRST", "PLATFORM", "POLICY", "POLICIES", "PEOPLE", "COUNTRY", "NATION", "NATIONAL", "WORLD", "TERM", "WASHINGTON", "FEDERAL", "GOVERNMENT", "INCLUDING", "PARTY", "PRESIDENT", "CONGRESS", "ADMINISTRATION", "WILL", "STATE", "UNITED", "STATES", "AMERICA", "AMERICAN", "AMERICANS"]
 	candidates_and_party_names = ["BIDEN", "HARRIS", "TRUMP", "DEMOCRATS", "REPUBLICANS", "DEMOCRAT", "REPUBLICAN"]
 	frequent_numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 
@@ -39,14 +39,14 @@ def generate_word_cloud(filename, color_map):
 	
 	# Word cloud word sizes are based solely on word frequency.
 	# Width and height match default PNG size for my computer and should probably be updated.
-	wordcloud = WordCloud(width=640, height=475, min_font_size=10, max_words=100, stopwords=stop_words, background_color="white", mode="RGB", relative_scaling=1, collocations=True, colormap=warm_color_map, random_state=42).generate(all_content)
+	wordcloud = WordCloud(width=640, height=475, min_font_size=12, max_words=max_words, stopwords=stop_words, background_color="white", mode="RGB", relative_scaling=1, collocations=True, colormap=warm_color_map, random_state=42).generate(all_content)
 
 	plt.imshow(wordcloud, interpolation='bilinear')
 	plt.axis("off")
 	plt.show()
 
 
-# Democratic: "democratic_2024_platform.pdf", "Blues"
-# Republican: "republican_2024_platform.pdf", "Reds"
-generate_word_cloud("democratic_2024_platform.pdf", "Blues")
-generate_word_cloud("republican_2024_platform.pdf", "Reds")
+# Democratic (Top 50): "democratic_2024_platform.pdf", "Blues", 50
+# Republican (Top 50): "republican_2024_platform.pdf", "Reds", 50
+generate_word_cloud("democratic_2024_platform.pdf", "Blues", 50)
+generate_word_cloud("republican_2024_platform.pdf", "Reds", 50)
