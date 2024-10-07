fd82e82ce6a4643d30ada143f7c044d95de937c3
maw1999 <30275207+maw1999@users.noreply.github.com>
2024-10-07 11:13:24 -0700

    Several improvements to word clouds
    Added more common stop words (e.g. American, policy, nation etc.)
    Set collocations to True since bigrams seem less noisy with additional stop word filtering
    Changed relative scaling to 1 so word sizes are directly proportional to frequency
    Added support for a warmer, more visible single-color map
    Updated font size and number of words (200 --> 100) shown to improve readability
    Created modular methods with better comments

--- 2024_party_platform_word_cloud.py
+++ 2024_party_platform_word_cloud.py
@@ -4,38 +4,49 @@ from os import path
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import warnings
 from typing_extensions import Protocol
 import pdfplumber
 
-# Democrat: democrat_2024_platform.pdf
-# Republican: republican_2024_platform.pdf
-print("Input platform PDF file: democrat_2024_platform.pdf or republican_2024_platform.pdf?\n")
-filename = input()
-print("Loading...\n")
+# Lifted from https://stackoverflow.com/questions/18926031/how-to-extract-a-subset-of-a-colormap-as-a-new-colormap-in-matplotlib
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
 
-all_content = ""
-with pdfplumber.open(filename) as pdf:
-	for page in pdf.pages:
-		paragraphs = page.extract_text(x_tolerance=1)
-		all_content += paragraphs
-		
-single_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "S"]
-odd_fragments_and_common_terms = ["RE", "ll", "PRESIDENT", "CONGRESS", "ADMINISTRATION"]
-past_and_present_candidates = ["BIDEN", "HARRIS", "TRUMP"]
-frequent_numbers = list()
+def generate_word_cloud(filename, color_map):
+	all_content = ""
+	with pdfplumber.open(filename) as pdf:
+		for page in pdf.pages:
+			paragraphs = page.extract_text(x_tolerance=1)
+			all_content += paragraphs
+			
+	# Filter out words not directly related to policy, common terms, and words that add noise to word maps.
+	single_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "S"]
+	odd_fragments_and_common_terms = ["RE", "ll", "NN", "AANNDD", "TTHHEE", "VE", "CHAPTER", "YEAR", "YEARS", "PLATFORM", "POLICY", "POLICIES", "PEOPLE", "COUNTRY", "NATION", "NATIONAL", "WORLD", "TERM", "WASHINGTON", "FEDERAL", "GOVERNMENT", "INCLUDING", "PARTY", "PRESIDENT", "CONGRESS", "ADMINISTRATION", "WILL", "STATE", "UNITED", "STATES", "AMERICA", "AMERICAN", "AMERICANS"]
+	candidates_and_party_names = ["BIDEN", "HARRIS", "TRUMP", "DEMOCRATS", "REPUBLICANS", "DEMOCRAT", "REPUBLICAN"]
+	frequent_numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 
-for i in range(100):
-	frequent_numbers.append(str(i))
+	for i in range(100):
+		frequent_numbers.append(str(i))
 
-stop_words = single_letters + odd_fragments_and_common_terms + past_and_present_candidates+ frequent_numbers + list(STOPWORDS)
+	stop_words = single_letters + odd_fragments_and_common_terms + candidates_and_party_names + frequent_numbers + list(STOPWORDS)
+	
+	cmap = plt.get_cmap(color_map)
+	warm_color_map = truncate_colormap(cmap, 0.75, 0.85)
+	
+	# Word cloud word sizes are based solely on word frequency.
+	# Width and height match default PNG size for my computer and should probably be updated.
+	wordcloud = WordCloud(width=640, height=475, min_font_size=10, max_words=100, stopwords=stop_words, background_color="white", mode="RGB", relative_scaling=1, collocations=True, colormap=warm_color_map, random_state=42).generate(all_content)
 
-# Democrat: BuPu
-# Republican: RgDy
-print("Word cloud color map: BuPu or RdGy?\n")
-color_map = input()
-wordcloud = WordCloud(width=650, height=475, min_font_size=7, stopwords=stop_words, background_color="white", mode="RGB", relative_scaling=0, collocations=False, colormap=color_map, random_state=42).generate(all_content)
+	plt.imshow(wordcloud, interpolation='bilinear')
+	plt.axis("off")
+	plt.show()
 
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+
+# Democratic: "democratic_2024_platform.pdf", "Blues"
+# Republican: "republican_2024_platform.pdf", "Reds"
+generate_word_cloud("democratic_2024_platform.pdf", "Blues")
+generate_word_cloud("republican_2024_platform.pdf", "Reds")
