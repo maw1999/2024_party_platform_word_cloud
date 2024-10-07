e6a82b6e3dba2210f2b75db3e06de0c64509d013
maw1999 <30275207+maw1999@users.noreply.github.com>
2024-10-06 20:59:10 -0700

    Add files via upload

--- 2024_party_platform_word_cloud.py
+++ 2024_party_platform_word_cloud.py
@@ -21,7 +21,7 @@ with pdfplumber.open(filename) as pdf:
 		all_content += paragraphs
 		
 single_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "S"]
-odd_fragments_and_common_terms = ["RE", "ll", "PRESIDENT", "CONGRESS"] 
+odd_fragments_and_common_terms = ["RE", "ll", "PRESIDENT", "CONGRESS", "ADMINISTRATION"]
 past_and_present_candidates = ["BIDEN", "HARRIS", "TRUMP"]
 frequent_numbers = list()
 
@@ -34,7 +34,7 @@ stop_words = single_letters + odd_fragments_and_common_terms + past_and_present_
 # Republican: RgDy
 print("Word cloud color map: BuPu or RdGy?\n")
 color_map = input()
-wordcloud = WordCloud(min_font_size=7, stopwords=stop_words, background_color="white", mode="RGB", relative_scaling=0, collocations=False, colormap=color_map, random_state=42).generate(all_content)
+wordcloud = WordCloud(width=650, height=475, min_font_size=7, stopwords=stop_words, background_color="white", mode="RGB", relative_scaling=0, collocations=False, colormap=color_map, random_state=42).generate(all_content)
 
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
